59d741b28921611ec28399744e6ac223351502bf
tio <dtrohymenko@gmail.com>
2024-05-24 09:39:01 +0200

    Add second_homework.py

---
+++ stings_methods.py
@@ -0,0 +1,7 @@
+from constants import MSG_INPUT_FIRST_NAME = 'Введіть ваше імя:'
+MSG_INPUT_LAST_NAME = 'Введіть ваше прізвище:'
+
+user_name = input(MSG_INPUT_FIRST_NAME)
+user_last_name = input(MSG_INPUT_LAST_NAME)
+
+print(user_first_name)
---
+++ strings_lesson.py
@@ -0,0 +1,14 @@
+student_name_1 = 'Dima'
+print(id(student_name_1))
+print(student_name_1)
+print(student_name_1)
+print(student_name_1)
+print(student_name_1)
+print(student_name_1)
+print(student_name_1)
+
+
+
+emoji = " 🐮 "
+print (emoji)
+
