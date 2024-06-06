5599510b3f5e63f9e003e4638df8f2732282ee1d
tio <dtrohymenko@gmail.com>
2024-06-06 19:58:36 +0200

    input for dish

--- functions.py
+++ functions.py
@@ -1,16 +1,18 @@
-def greet_someone(username):
-    return f"Вітаємо тебе, шановний {username}"
+def greet_user_with_h1(username):
+    result = f"<h1>Вітаємо тебе, шановний {username}</h1>"
+    return result
 
 
 username = input("Введіть ваше ім'я: ").title()
-html_user = greet_someone(username)
+html_user = greet_user_with_h1(username)
 print(html_user)
 
 
 # geometry_function
-def count_square(length, width):
+def calculations_area_the_rectangle(length, width):
     result = length * width
     return result
 
-area = count_square(length=10, width=5)
-print(f"Площа прямокутника: {area}")
+
+area = calculations_area_the_rectangle(length=10, width=5)
+print(f"<h1> Площа прямокутника: {area}</h1>")
