25c4f2249290c1b7745ff81449216d70897ad988
tio <dtrohymenko@gmail.com>
2024-06-24 15:15:06 +0200

    input for dish

--- lists.py
+++ lists.py
@@ -4,7 +4,7 @@ url_ = "http://api.open-notify.org/astros.json"
 responce = requests.get(url_)
 data = responce.json()
 
-astros_list = [person["name"] for person in data["people"]]
+astros_list = [astronaut["name"] for astronaut in data["people"]]
 for astronaut in astros_list:
     print(astronaut)
 
@@ -12,8 +12,17 @@ url_second = "https://dummyjson.com/users"
 responce_second = requests.get(url_second)
 data_second = responce_second.json()
 
-user_age_special = [f" {user['first_name']} {user['last_name']}" for user in data_second[" users "] if
-                    user["age"] is 28]
-for user in user_age_special:
-    print(user_age_special)
+
+#for user in data_second:
+    #if user["age"] == 28:
+        #print(f"{user['firstName']}{user['lastName']}")
+
+
+users_28 = [f"{user['firstName']} {user['lastName']}"
+            for user in data_second['users']
+            if user['age'] is 28]
+for user in users_28:
+    print(user)
+
+
 
