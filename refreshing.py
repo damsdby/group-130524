fb218abd9c6f7b3aac6e98491d7e7717740f4206
tio <dtrohymenko@gmail.com>
2024-09-21 11:01:54 +0200

    input for dish

--- refreshing.py
+++ refreshing.py
@@ -1,12 +1,11 @@
-str_1 = "SOme texttt  with     2367d d jd"
-no_space = str_1.replace(' ', '')
-print(no_space)
-
-
 def is_palindrome(s):
     s_lower = s.lower()
     return s_lower == s_lower[::-1]
 
+
+str_1 = "SOme texttt  with     2367d d jd"
+no_space = str_1.replace(' ', '')
+print(no_space)
 
 if is_palindrome(str_1):
     print("Так")
