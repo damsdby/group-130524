0247804c08c374f94d78b7c1969131f025100aff
tio <dtrohymenko@gmail.com>
2024-09-24 14:13:50 +0200

    input for dish

--- task.py
+++ task.py
@@ -6,12 +6,10 @@ ba = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 arr = [6, 6, 2, 1, 5, 8, 13, 21, 34, 55, 89]
 bar = [1, 18, 3, 4, 5, 9, 7, 8, 9, 10, 11, 12, 13]
 
-unique_arr = [x for x in arr if arr.count(x) == 1]
-unique_list2 = [x for x in bar if bar.count(x) == 1]
-
 for el in as_list:
     if el < 5:
         print(el)
 
 print(list(set(as_list_second) & set(ba)))
-print(list(set(unique_arr) & set(unique_list2)))
+unique_values = list(set(arr) ^ set(bar))
+print(unique_values)
